40e9f0f6b467aa180446b2691694a4aa7a95839b
Zafer İntepe <108065413+IntepeZafer@users.noreply.github.com>
2025-02-10 01:09:44 +0300

    Score Text Commit

---
+++ Assets/Script/ScoreText.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using TMPro;
+public class ScoreText : MonoBehaviour
+{
+    public TextMeshProUGUI scoreText;
+    
+    public void SetScore(int value)
+    {
+        scoreText.text = value.ToString();
+    }
+}
